4ad793056b981a273da3ffa84f7c3b45eb70aae9
PDW GROUP <support@PDWs-Air.home>
2025-03-02 14:43:42 +0000

    jenkins-sharded-librar

--- script.groovy
+++ script.groovy
@@ -1,4 +1,26 @@
 
+// def deployJar(){
+//     echo "deploying the application..."
+//     // echo "deploying version ${params.VERSION}"
+// }
+
+// return this
+
+
+def buildJar(){
+    echo "building the application..."
+    sh "mvn package "
+}
+
+def buildImage(){
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: "docker-hub-creds", passwordVariable: "PASS", usernameVariable: "USER")]){
+        sh "docker build -t santana20095/demo-app:jma-2.0 ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push santana20095/demo-app:jma-2.0"
+    }
+}
+
 def deployJar(){
     echo "deploying the application..."
     // echo "deploying version ${params.VERSION}"
